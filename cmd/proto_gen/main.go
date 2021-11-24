fd3ecd74ba5e683e0cbde27df0eb90e58f4bfed9
xhsf <827032783@qq.com>
2021-11-24 23:48:50 +0800

    1

---
+++ cmd/proto_gen/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+var protoFilePaths []string
+
+func main() {
+	processDir("api")
+	args := []string{"--go_out=paths=source_relative:."}
+	args = append(args, protoFilePaths...)
+	protocCommand := exec.Command("protoc", args...)
+	protocCommand.Stdout = os.Stdout
+	protocCommand.Stderr = os.Stderr
+	if err := protocCommand.Run(); err != nil {
+		log.Fatal("生成proto失败", err)
+	}
+}
+
+func processDir(dir string) {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range files {
+		fileName := fmt.Sprintf("%s/%s", dir, file.Name())
+		if file.IsDir() {
+			processDir(fileName)
+		} else if strings.HasSuffix(fileName, ".proto") {
+			protoFilePaths = append(protoFilePaths, fileName)
+		}
+	}
+}
